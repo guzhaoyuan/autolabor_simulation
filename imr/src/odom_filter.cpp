57b56c105eeceb920de801d1cb30557623e7805a
guzhaoyuan <guzhaoyuan14@gmail.com>
2020-04-19 18:33:38 -0400

    Get sensor data processing correct.

--- imr/src/odom_filter.cpp
+++ imr/src/odom_filter.cpp
@@ -5,6 +5,7 @@
 #include <tf/transform_listener.h>
 #include <tf/transform_broadcaster.h>
 #include <apriltag_ros/AprilTagDetectionArray.h>
+#include <tf_conversions/tf_eigen.h>
 
 class OdomFilter {
  public:
@@ -21,45 +22,52 @@ class OdomFilter {
     private_node.getParam("distance_UWB_to_base", distance_UWB_to_base);
     private_node.getParam("distance_camera_to_base", distance_camera_to_base);
 
-//    init();
     run();
+    init();
   }
 
   void init() {
-    tf::StampedTransform map_to_odom_link;
-    while (1) { // TODO: This is a bad style, change this later.
-      try {
-        listener.lookupTransform("/odom", "/map",
-                                 ros::Time(0), map_to_odom_link);
-        break;
-      } catch (tf::TransformException ex) {
-        // Only start odometry if global orientation is detected.
-        ros::Duration(1).sleep();
-      }
-    }
-    fiducial0_W.topLeftCorner<3,3>() <<
-        1, 0, 0,
-        0, 1, 0,
-        0, 0, 1;
-    fiducial0_W.topRightCorner<3,1>() << 0, 2.44, 0;
+    fiducial0_W = Eigen::Translation3d(Eigen::Vector3d{0, 2.44, 0})
+        * Eigen::AngleAxisd(M_PI/2, Eigen::Vector3d::UnitZ())
+        * Eigen::AngleAxisd(M_PI/2, Eigen::Vector3d::UnitX());
+
+//    tf::poseEigenToTF(fiducial0_W, tf_WF);
+
+//    tf_BC.setIdentity();
+//    tf_BC.setOrigin(tf::Vector3{0.03, 0.01, 0});
+
+//    // Wait for initialization from fiducial.
+//    tf::StampedTransform map_to_odom_link;
+//    while (ros::ok()) { // TODO: This is a bad style, change this later.
+//      try {
+//        listener.lookupTransform("/odom", "/map",
+//                                 ros::Time(0), map_to_odom_link);
+//        break;
+//      } catch (tf::TransformException ex) {
+//        // Only start odometry if global orientation is detected.
+//        ROS_INFO("Wait for initialization from fiducial.");
+//        ros::Duration(0.1).sleep();
+//      }
+//    }
+//    ROS_INFO("Map to Odom initialized by fiducial.");
   }
 
   void run() {
     vo_odom = node.advertise<nav_msgs::Odometry>("t265/odom", 10);
     uwb_odom = node.advertise<nav_msgs::Odometry>("dwm/odom", 10);
     imu_odom = node.advertise<sensor_msgs::Imu>("imu/odom", 10);
+    tag_odom = node.advertise<nav_msgs::Odometry>("tag/odom", 10);
 
     vo_odom_listener = node.subscribe("t265/odom/sample",
         100, &OdomFilter::vo_odom_callback, this);
-    uwb_odom_listener = node.subscribe("dwm_odom", 100, &OdomFilter::uwb_odom_callback, this);
-    luwb_pos_listener = node.subscribe("/lhs/dwm1001/tag",
+//    uwb_odom_listener = node.subscribe("dwm_odom", 100, &OdomFilter::uwb_odom_callback, this);
+    luwb_pos_listener = node.subscribe("lhs/dwm1001/tag",
                                                           10, &OdomFilter::uwb_lpos_callback, this);
-    ruwb_pos_listener = node.subscribe("/rhs/dwm1001/tag",
+    ruwb_pos_listener = node.subscribe("rhs/dwm1001/tag",
                                                           10, &OdomFilter::uwb_rpos_callback, this);
     imu_listener = node.subscribe("rtabmap/imu", 100, &OdomFilter::imu_odom_callback, this);
-    fiducial_listener = node.subscribe("tag_detections", 10,
+    fiducial_listener = node.subscribe("tag_detections", 100,
         &OdomFilter::fiducial_CB, this);
-
   }
 
   void vo_odom_callback(const nav_msgs::Odometry::ConstPtr& msg) {
@@ -137,6 +145,9 @@ class OdomFilter {
   }
 
   void uwb_lpos_callback(const geometry_msgs::Point::ConstPtr& msg) {
+    if (first_f) // Do nothing if fiducial not initialized.
+      return;
+
     if (first_luwb < 5) {
       first_luwb ++;
       first_luwb_pos = Eigen::Vector3d(msg->x, msg->y, msg->z);
@@ -150,24 +161,24 @@ class OdomFilter {
 
     // Try to rotate the UWB sensor reading to align with the map frame.
     // Assume the first UWB reading is at the odom frame. To make alignment, the initial odom frame angle is guessed.
-    Eigen::Vector3d position_W = Eigen::Vector3d(msg->x, msg->y, msg->z) - first_uwb_pos;
+    Eigen::Vector3d position_W = Eigen::Vector3d(msg->x, msg->y, msg->z) - first_luwb_pos;
+
     Eigen::Vector3d position_UWB = Eigen::AngleAxisd(init_odom_yaw, Eigen::Vector3d::UnitZ()).toRotationMatrix() *
         position_W;
 
     tf::StampedTransform odom_to_base_link;
     try {
-      listener.lookupTransform("/base_link", "/odom",
+      listener.lookupTransform("/odom", "/base_link",
                                ros::Time(0), odom_to_base_link);
     } catch (tf::TransformException ex) {
       odom_to_base_link.setIdentity();
     }
     tf::Quaternion q = odom_to_base_link.getRotation();
-    Eigen::Quaterniond q_WO(q.w(),q.x(),q.y(),q.z());
-    Eigen::Matrix3d R_WO = q_WO.toRotationMatrix();
+    Eigen::Quaterniond q_OB(q.w(),q.x(),q.y(),q.z());
+    Eigen::Matrix3d R_OB = q_OB.toRotationMatrix();
     // Compute the base_link in uwb_odom frame by adding offset from UWB.
-    Eigen::Vector3d position_base = position_UWB + R_WO.transpose() *
+    Eigen::Vector3d position_base = position_UWB + R_OB.transpose() *
         Eigen::Vector3d(distance_UWB_to_base_X, distance_UWB_to_base_Y, 0);
-
     modified_msg.pose.pose.position.x = position_base(0);
     modified_msg.pose.pose.position.y = position_base(1);
     modified_msg.pose.pose.position.z = 0;
@@ -242,8 +253,9 @@ class OdomFilter {
   }
 
   void imu_odom_callback(const sensor_msgs::Imu::ConstPtr& msg) {
+    // Throw away first several imu data.
     if (first_imu < 5) {
-      // Throw away first several imu data.
+      // Remember initial yaw angle and compensate later.
       imu_init_val = GetEulerZ(msg);
       q_first = Eigen::Quaterniond(msg->orientation.w, msg->orientation.x, msg->orientation.y, msg->orientation.z);
       first_imu ++;
@@ -283,11 +295,49 @@ class OdomFilter {
       return;
 
     // Get the first detection between camera C and fiducial F.
-    tf::Transform tf_transform_CF;
-    tf::poseMsgToTF(tag_array->detections[0].pose.pose.pose, tf_transform_CF);
+    tf::Transform tf_CF;
+    tf::poseMsgToTF(tag_array->detections[0].pose.pose.pose, tf_CF);
+    tf_broadcaster_.sendTransform(tf::StampedTransform(tf_CF,
+        ros::Time::now(), "t265_fisheye1_optical_frame", "detected_fiducial"));
 
-    tf_broadcaster_.sendTransform(tf::StampedTransform(tf_transform_CF,
-        ros::Time::now(), "t265_fisheye1_optical_frame", "fiducial"));
+    tf::StampedTransform tf_WF;
+    tf::StampedTransform tf_BC;
+    try {
+      listener.lookupTransform("/map", "/fiducial0",
+                               ros::Time(0), tf_WF);
+      listener.lookupTransform("/base_link", "/t265_fisheye1_optical_frame",
+                               ros::Time(0), tf_BC);
+    } catch (tf::TransformException ex) {
+      return;
+    }
+
+    tf::Transform tf_BF = tf_BC * tf_CF;
+    if (first_f) {
+      tf::Transform tf_WB = tf_WF * tf_BF.inverse();
+      tf_broadcaster_.sendTransform(tf::StampedTransform(tf_WB,
+          ros::Time::now(), "map", "odom"));
+      double roll, pitch, yaw;
+      tf_WB.getBasis().getRPY(roll, pitch, init_odom_yaw);
+      ROS_INFO("[Fiducial]: Map to Odom initialized. Init yaw: %.1f.",
+          init_odom_yaw);
+      first_f = false;
+      return;
+    }
+
+    try {
+      tf::StampedTransform tf_OB;
+      listener.lookupTransform("/odom", "/base_link",
+                               ros::Time(0), tf_OB);
+
+      tf::Transform tf_transform_WO = tf_WF * tf_BF.inverse() * tf_OB.inverse();
+      tf_broadcaster_.sendTransform(tf::StampedTransform(tf_transform_WO,
+        ros::Time::now(), "map", "odom"));
+      nav_msgs::Odometry tag_msg;
+
+      tag_odom.publish(tag_msg);
+    } catch (tf::TransformException ex) {
+      return;
+    }
   }
 
   ros::NodeHandle node;
@@ -295,6 +345,7 @@ class OdomFilter {
   ros::Publisher vo_odom;
   ros::Publisher uwb_odom;
   ros::Publisher imu_odom;
+  ros::Publisher tag_odom;
   ros::Subscriber vo_odom_listener;
   ros::Subscriber uwb_odom_listener;
   ros::Subscriber luwb_pos_listener;
@@ -306,6 +357,7 @@ class OdomFilter {
   tf::TransformBroadcaster tf_broadcaster_;
 
   bool first_vo = true;
+  bool first_f = true;
   int first_uwb = 0;
   int first_luwb = 0;
   int first_ruwb = 0;
@@ -317,7 +369,7 @@ class OdomFilter {
   double vo_vel_cov = 0.0;
   double uwb_pos_cov = 0.0;
   double imu_ori_cov = 0.01;
-  double init_odom_yaw = -0.75 * M_PI;
+  double init_odom_yaw = -1 * M_PI;
   double init_imu_yaw = -0.75 * M_PI;
   double distance_UWB_to_base = 0.308;
   double distance_UWB_to_base_X = -0.02;
@@ -328,7 +380,8 @@ class OdomFilter {
   Eigen::Vector3d first_uwb_pos = Eigen::Vector3d::Zero();
   Eigen::Vector3d first_luwb_pos = Eigen::Vector3d::Zero();
   Eigen::Vector3d first_ruwb_pos = Eigen::Vector3d::Zero();
-  Eigen::Matrix4d fiducial0_W;
+  Eigen::Affine3d fiducial0_W;
+//  tf::Transform tf_WF;
 };
 
 
