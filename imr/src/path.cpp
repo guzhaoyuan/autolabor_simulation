458d7d056216eb8183c5fec8082518de819e11ef
guzhaoyuan <guzhaoyuan14@gmail.com>
2020-06-23 03:29:17 -0400

    Add visual odometry raw data visualization.
    The VO data in one of the bag definitely has huge drift. Will check more bags for verification.

--- imr/src/path.cpp
+++ imr/src/path.cpp
@@ -7,6 +7,7 @@
 #include "nav_msgs/Path.h"
 #include <xlnt/xlnt.hpp>
 #include <Eigen/Dense>
+#include <nav_msgs/Odometry.h>
 
 class PathVisualization {
  public:
@@ -152,10 +153,54 @@ class GroundTruthVisualization {
   double delay_start;
 };
 
+class VOVisualization {
+ public:
+  VOVisualization () {
+    ros::NodeHandle private_node("~");
+    private_node.param("rate", rate_, 1);
+    private_node.param("delay_start", delay_start, 1.0);
+    private_node.param<std::string>("relative_frame", relative_frame,
+                                    "odom");
+    ros::Duration(delay_start).sleep();
+    path_pub_ = nh_.advertise<nav_msgs::Path>("vo_path", 10);\
+    pub_path_timer_ = nh_.createTimer(ros::Duration(1.0/rate_),
+                                      &VOVisualization::pubPathCallback, this);
+    vo_odom_listener_ = nh_.subscribe("t265/odom/sample",
+                                      100, &VOVisualization::vo_odom_callback, this);
+  }
+
+  void pubPathCallback(const ros::TimerEvent &event) {
+    path_pub_.publish(path_);
+  }
+
+  void vo_odom_callback(const nav_msgs::Odometry::ConstPtr& msg) {
+    path_.header.frame_id = relative_frame;
+    path_.header.stamp = ros::Time::now();
+
+    geometry_msgs::PoseStamped p;
+    p.pose.position.x = msg->pose.pose.position.x;
+    p.pose.position.y = msg->pose.pose.position.y;
+
+    path_.poses.push_back(p);
+  }
+
+  ros::NodeHandle nh_;
+  ros::Publisher path_pub_;
+  ros::Timer pub_path_timer_;
+  ros::Subscriber vo_odom_listener_;
+  int rate_;
+  nav_msgs::Path path_;
+  double delay_start;
+  std::string relative_frame;
+};
+
 int main (int argc, char **argv) {
   ros::init(argc, argv, "path_visualization_node");
+
   PathVisualization pv;
   GroundTruthVisualization gtv;
+  VOVisualization vov;
+
   ros::spin();
   return 0;
 }
